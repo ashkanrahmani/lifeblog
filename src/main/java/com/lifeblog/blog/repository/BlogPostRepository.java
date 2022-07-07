56242141e08831967055267be6c307424322de40
Ashkan Rahmani <ashkan.rahmani@gmail.com>
2022-07-07 12:08:15 +0430

    code review

--- src/main/java/com/lifeblog/blog/repository/BlogPostRepository.java
+++ src/main/java/com/lifeblog/blog/repository/BlogPostRepository.java
@@ -3,7 +3,6 @@ package com.lifeblog.blog.repository;
 import com.lifeblog.blog.entity.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
