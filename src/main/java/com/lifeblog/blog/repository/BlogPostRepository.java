dd49bc8db9c3c0375193dda9041f0fb0f35a4ae2
Ashkan Rahmani <ashkan.rahmani@gmail.com>
2022-07-07 12:07:02 +0430

    add @repo

--- src/main/java/com/lifeblog/blog/repository/BlogPostRepository.java
+++ src/main/java/com/lifeblog/blog/repository/BlogPostRepository.java
@@ -2,6 +2,9 @@ package com.lifeblog.blog.repository;
 
 import com.lifeblog.blog.entity.BlogPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
 }
