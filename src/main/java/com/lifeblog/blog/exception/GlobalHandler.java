0e9a03ef5bd05b0f00ca158f9936b1ee17290086
Ashkan Rahmani <ashkan.rahmani@gmail.com>
2022-07-08 19:16:31 +0430

    argument validation

--- src/main/java/com/lifeblog/blog/exception/GlobalHandler.java
+++ src/main/java/com/lifeblog/blog/exception/GlobalHandler.java
@@ -1,16 +1,25 @@
 package com.lifeblog.blog.exception;
 
 import com.lifeblog.blog.controller.payload.ErrorDetails;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalHandler {
+public class GlobalHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -36,7 +45,7 @@ public class GlobalHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleException(Exception exception, WebRequest webRequest) {
+    public ResponseEntity<ErrorDetails> handleOtherException(Exception exception, WebRequest webRequest) {
 
         ErrorDetails errorDetails = new ErrorDetails();
         errorDetails.setDate(new Date());
@@ -44,5 +53,19 @@ public class GlobalHandler {
         errorDetails.setDetails(webRequest.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+//    @NonNull
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
