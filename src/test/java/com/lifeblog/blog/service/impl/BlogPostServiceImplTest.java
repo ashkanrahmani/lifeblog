32e15a10f9f702d0d1d1200da8837e979ad997f4
Ashkan Rahmani <ashkan.rahmani@gmail.com>
2022-07-17 22:21:28 +0430

    JUnit add BlogPostServiceImplTest

---
+++ src/test/java/com/lifeblog/blog/service/impl/BlogPostServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.lifeblog.blog.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlogPostServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllBlogPosts() {
+        fail("not yest");
+    }
+
+    @Test
+    void getBlogPostById() {
+    }
+
+    @Test
+    void deleteBlogPostById() {
+    }
+}
